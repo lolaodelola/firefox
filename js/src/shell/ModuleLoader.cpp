34bcedace6451e68c222f767de7a51492b7a974e
André Bargull <andre.bargull@gmail.com>
2026-03-24 06:09:57 +0000

    Bug 2024289: Use Immutable Uint8Array for import-bytes proposal support in shell module loader. r=dminor
    Using an immutable Uint8Array allows to pass the new test262 for the
    import-bytes proposal, added in bug 2024290.
    
    Changes:
    - Adds `FileAsImmutableTypedArray` which is similar to `FileAsTypedArray`, but
      returns an immutable Uint8Array.
    - Don't check for shared memory in `FileAsTypedArray`, this case can never happen.
    - Update test to expect immutable Uint8Array and add a CCW test, too.
    - Simplify `|jit-test|` to use `--enable-import-bytes`, which works even in beta
      or release, so we don't need the `--fuzzing-safe` workaround.
    
    Differential Revision: https://phabricator.services.mozilla.com/D288453

--- js/src/shell/ModuleLoader.cpp
+++ js/src/shell/ModuleLoader.cpp
@@ -528,15 +528,12 @@ JSObject* ModuleLoader::loadAndParse(JSContext* cx, HandleString pathArg,
       return nullptr;
     }
 
-    auto* bytesArray = FileAsTypedArray(cx, resolvedPath);
-    if (!bytesArray) {
+    auto* typedArray = FileAsImmutableTypedArray(cx, resolvedPath);
+    if (!typedArray) {
       return nullptr;
     }
-    JS::Rooted<JS::Value> defaultExport(cx, ObjectValue(*bytesArray));
+    JS::Rooted<JS::Value> defaultExport(cx, ObjectValue(*typedArray));
 
-    // TODO: The spec requires that the ArrayBuffer be immutable.
-    // Immutable ArrayBuffers (see bug 1952253) are still only a Stage 2.7
-    // proposal, once they are Stage 3, we can fix this.
     module = JS::CreateDefaultExportSyntheticModule(cx, defaultExport);
     if (!module) {
       return nullptr;
