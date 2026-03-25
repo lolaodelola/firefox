1729b70ce46b258d3b0fde56807a9d4cc46e2c43
Jon Coppeard <jcoppeard@mozilla.com>
2026-03-25 09:25:57 +0000

    Bug 2024175 - Part 2: Add lightweight lock class r=sfink,jandem
    This adds a lock based on rust's parking lock mutex. It has some big
    restrictions to simplify the implementation, the main one being that at most
    two threads are allowed to compete for the mutex.
    
    Differential Revision: https://phabricator.services.mozilla.com/D288457

--- js/src/vm/MutexIDs.h
+++ js/src/vm/MutexIDs.h
@@ -62,6 +62,7 @@
   _(GCDelayedMarkingLock, 500)        \
   _(BufferAllocator, 500)             \
   _(GeckoProfilerScriptSources, 500)  \
+  _(GCLightLock, 500)                 \
                                       \
   _(SharedImmutableStringsCache, 600) \
   _(IrregexpLazyStatic, 600)          \
