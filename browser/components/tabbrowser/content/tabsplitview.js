ada4172b8cbd2f86d0c87ab40ed0c30cced57f7d
Sandor Molnar <smolnar@mozilla.com>
2026-04-07 18:43:19 +0300

    Revert "Bug 2026723 - Fix issue with restoring splitview tabs and other cleanup r=sessionstore-reviewers,tabbrowser-reviewers,sfoster,nsharpley" for causing Mn failures @ test_splitview_restore.py
    This reverts commit ae0987e3d5d3b7061412e6dc2feb36b57bc447af.

--- browser/components/tabbrowser/content/tabsplitview.js
+++ browser/components/tabbrowser/content/tabsplitview.js
@@ -224,9 +224,11 @@
     /**
      * Show all Split View tabs in the content area.
      */
-    #activate() {
+    #activate(skipShowPanels = false) {
       updateUrlbarButton.arm();
-      gBrowser.showSplitViewPanels(this.#tabs);
+      if (!skipShowPanels) {
+        gBrowser.showSplitViewPanels(this.#tabs);
+      }
       this.container.dispatchEvent(
         new CustomEvent("TabSplitViewActivate", {
           detail: { tabs: this.#tabs, splitview: this },
@@ -324,7 +326,7 @@
         }
       }
 
-      if (this.hasActiveTab) {
+      if (this.hasActiveTab || isSessionRestore) {
         this.#activate();
       }
       // Attempt to update uriCount metric using the resulting tabs collection,
