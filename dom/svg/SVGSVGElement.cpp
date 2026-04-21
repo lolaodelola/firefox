66b064bc46a9dfc16ca14e0f06efb8817f04b715
longsonr <longsonr@gmail.com>
2026-04-21 12:01:06 +0000

    Bug 2032770 - Update implementation of deselectAll to match latest SVG 2 definition r=emilio
    See https://github.com/w3c/svgwg/issues/1095
    
    Differential Revision: https://phabricator.services.mozilla.com/D294911

--- dom/svg/SVGSVGElement.cpp
+++ dom/svg/SVGSVGElement.cpp
@@ -232,10 +232,13 @@ void SVGSVGElement::SetCurrentTime(float seconds) {
 }
 
 void SVGSVGElement::DeselectAll() {
-  nsIFrame* frame = GetPrimaryFrame();
-  if (frame) {
-    RefPtr<nsFrameSelection> frameSelection = frame->GetFrameSelection();
-    frameSelection->ClearNormalSelection();
+  if (Document* doc = GetComposedDoc()) {
+    if (RefPtr<PresShell> presShell = doc->GetPresShell()) {
+      if (RefPtr<Selection> docSel =
+              presShell->GetCurrentSelection(SelectionType::eNormal)) {
+        docSel->RemoveAllRanges(IgnoreErrors());
+      }
+    }
   }
 }
 
