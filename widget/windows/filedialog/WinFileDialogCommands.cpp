0e5a02da22b697946f4fdb1031c96b66a65b5ce8
pstanciu <pstanciu@mozilla.com>
2026-04-28 21:43:24 +0300

    Revert "Bug 2028476: Increase worker thread stack size in Windows file picker utility process r=win-reviewers,gstoll" for causing build bustages @ WinFileDialogCommands.cpp
    This reverts commit 7cfeb385b23d37b0284461c251b949826b89a8a1.

--- widget/windows/filedialog/WinFileDialogCommands.cpp
+++ widget/windows/filedialog/WinFileDialogCommands.cpp
@@ -20,10 +20,6 @@
 
 namespace mozilla::widget::filedialog {
 
-// The default Windows thread stack size of 1Mb may not be enough for some
-// DLLs that are added to the file dialog by 3rd party apps.  Try 4Mb.
-const size_t kWindowsFileDialogStackSize = 4 * 1024 * 1024;
-
 const char* Error::KindName(Error::Kind kind) {
   switch (kind) {
     case LocalError:
@@ -360,9 +356,8 @@ RefPtr<Promise<Res>> SpawnFileDialogThread(const char (&where)[N],
 
   RefPtr<nsIThread> thread;
   {
-    nsresult rv = NS_NewNamedThread(
-        "File Dialog", getter_AddRefs(thread), nullptr,
-        {.isUiThread = true, .stackSize = kWindowsFileDialogStackSize});
+    nsresult rv = NS_NewNamedThread("File Dialog", getter_AddRefs(thread),
+                                    nullptr, {.isUiThread = true});
     if (NS_FAILED(rv)) {
       return Promise<Res>::CreateAndReject(
           MOZ_FD_LOCAL_ERROR("NS_NewNamedThread", (HRESULT)rv), where);
