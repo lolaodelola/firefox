62bfbbfd85725d7aed65a364dfe200bb2ad3c4fd
Jeff Boek <jboek@mozilla.com>
2026-04-28 18:40:34 +0000

    Bug 2034666 - Adds a real implementation to lib-bookmarks-file r=android-reviewers,sfamisa
    Differential Revision: https://phabricator.services.mozilla.com/D296270

--- mobile/android/android-components/components/concept/bookmarks-file/src/main/java/mozilla/components/concept/bookmarks/file/BookmarksFileImporter.kt
+++ mobile/android/android-components/components/concept/bookmarks-file/src/main/java/mozilla/components/concept/bookmarks/file/BookmarksFileImporter.kt
@@ -11,18 +11,38 @@ import android.net.Uri
  */
 fun interface BookmarksFileImporter {
     /**
-     * Represents the number of bookmarks that were imported.
+     * The result of a bookmarks file import.
      *
+     * @property guid The guid of the root of the imported bookmark tree.
      * @property count The number of bookmarks imported.
      */
-    @JvmInline
-    value class NumberOfBookmarks(val count: Int)
+    data class ImportResult(val guid: String, val count: Int)
 
     /**
      * Imports bookmarks from the file at the given [uri].
      *
      * @param uri The URI of the file to import bookmarks from.
-     * @return A [Result] containing [NumberOfBookmarks] on success or an exception on failure.
+     * @return A [Result] containing [ImportResult] on success or an exception on failure.
      */
-    suspend fun importBookmarksFromUri(uri: Uri): Result<NumberOfBookmarks>
+    suspend fun importBookmarksFromUri(uri: Uri): Result<ImportResult>
+
+    companion object {
+        /**
+         * Creates a [BookmarksFileImporter] that always returns a successful [Result].
+         *
+         * @param result The [ImportResult] to return on every import.
+         */
+        fun alwaysSuccess(
+            result: ImportResult = ImportResult(guid = "", count = 0),
+        ): BookmarksFileImporter = BookmarksFileImporter { Result.success(result) }
+
+        /**
+         * Creates a [BookmarksFileImporter] that always returns a failed [Result].
+         *
+         * @param exception The [Exception] to return on every import.
+         */
+        fun alwaysFailure(
+            exception: Exception = IllegalStateException("Import failed"),
+        ): BookmarksFileImporter = BookmarksFileImporter { Result.failure(exception) }
+    }
 }
